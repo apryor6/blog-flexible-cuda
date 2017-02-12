84c4a3c50f13d5c466846b74d257d8b52729bedb
AJ Pryor <apryor6@gmail.com>
2017-02-12 06:09:48 -0800

    xfer

--- ArrayPow2_CUDA.cu
+++ ArrayPow2_CUDA.cu
@@ -4,6 +4,29 @@
 #include "cuda_runtime_api.h"
 #define BLOCK_SIZE 1024
 
+template <class T>
+__global__ void pow2(T* in, T* out, size_t N){
+    int idx = threadIdx.x + blockIdx.x*blockDim.x;
+    if (idx < N)out[idx] = in[idx] * in[idx];
+}
+
+template <class T>
+void ArrayPow2_CUDA(const Array2D<T>& in, Array2D<T>& result) {
+    Array2D< Cutype<T> > in_d(in);
+    size_t N = in.size();
+    pow2 <<< (N - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>> (in_d.begin(), in_d.begin(), in.size());
+    //double* in2 = in_d.begin();
+    //double* out2 = in2;
+    //pow2 <<< (N - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>> (in2, out2, in.size());
+    T* arr_h = in.begin();
+    T* arr_d = in_d.begin();
+    cudaMemcpy(arr_h, arr_d, sizeof(T) * N, cudaMemcpyDeviceToHost);
+}
+
+template void ArrayPow2_CUDA<double>;
+template void pow2<double>;
+
+/*
 __global__ void pow2(double* in, double* out, size_t N){
     int idx = threadIdx.x + blockIdx.x*blockDim.x;
     if (idx < N)out[idx] = in[idx] * in[idx];
@@ -20,3 +43,4 @@ void ArrayPow2_CUDA(const Array2D<double>& in, Array2D<double>& result) {
     double* arr_d = in_d.begin();
     cudaMemcpy(arr_h, arr_d, sizeof(double) * N, cudaMemcpyDeviceToHost);
 }
+*/
--- ArrayPow2_CUDA.cuh
+++ ArrayPow2_CUDA.cuh
@@ -1,4 +1,5 @@
 #include "Array2D_CUDA.h"
 #include "Array2D.h"
 
-void ArrayPow2_CUDA(const Array2D<double>& in, Array2D<double>& result);
+template <class T>
+void ArrayPow2_CUDA(const Array2D<T>& in, Array2D<T>& result);
