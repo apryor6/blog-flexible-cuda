49f5a9445a5ea673e9c8b86bfcfb7edd9468bc5e
apryor6@gmail.com <>
2017-02-13 08:54:55 -0800

    bug fix

--- ArrayPow2_CUDA.cu
+++ ArrayPow2_CUDA.cu
@@ -4,9 +4,9 @@
 #include "cuda_runtime_api.h"
 #include <iostream>
 #define BLOCK_SIZE 1024
-
 template <class T>
 __global__ void pow2(T* in, T* out, size_t N){
+
     int idx = threadIdx.x + blockIdx.x*blockDim.x;
     if (idx < N)out[idx] = in[idx] * in[idx];
 }
@@ -15,9 +15,12 @@ template <class T>
 void ArrayPow2_CUDA(Array2D<T>& in, Array2D<T>& result) {
     std::cout << "Using the GPU version\n";
     Array2D< Cutype<T> > in_d(in);
+    std::cout << "in[0] = " << *in.begin() << std::endl;
     size_t N = in.size();
+    std::cout << "N = " << N << std::endl;
     pow2 <<< (N - 1) / BLOCK_SIZE + 1, BLOCK_SIZE >>> (in_d.begin(), in_d.begin(), in.size());
-    cudaMemcpy(in.begin(), in_d.begin(), sizeof(T) * N, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    cudaMemcpy(result.begin(), in_d.begin(), sizeof(T) * N, cudaMemcpyDeviceToHost);
 }
 
 
